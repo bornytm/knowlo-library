23898d4db95d4e72f67bace8d297d215f25baae6
Tim <bornytm@gmail.com>
2020-04-07 18:07:50 -0400

    update template

--- CRUD/generateMeta.js
+++ CRUD/generateMeta.js
@@ -5,6 +5,7 @@ module.exports = function(linkPreview, db){
     linkPreview.get('*', catchAll);   
 
     function catchAll(req,res) { 
+        console.log(req.url)
         res.send(meta())
     }
 
@@ -14,23 +15,24 @@ module.exports = function(linkPreview, db){
                + "WITH resource "
                + "OPTIONAL MATCH (resource)-[p:HAS_PROPERTY]->(prop:prop)-[plang:HAS_TRANSLATION ]->(ptrans:translation) "
                + "WHERE p.order=1 AND plang.languageCode IN [ {languageCode} , 'en' ] and prop.type='title' "
-               + "RETURN resource.thumb as url, ptrans.value as title"
+               + "RETURN resource.thumb as image, ptrans.value as title"
 
         db.query(cypher, {uid: req.params.uid, languageCode: req.query.languageCode || 'en'},function(err, result) {
             if(err){console.log(err)}
             if(result.length == 0){
                 res.send(meta())
             } else {
-                res.send(meta(result[0].title, undefined, result[0].url))
+                res.send(meta(result[0].title, undefined, result[0].image, 'https://www.knowlo.io' + req.url))
             }
         })
     }
 
-    function meta(title, description, url){
+    function meta(title, description, imageUrl, url){
         // defaults
         let ttitle = title || 'Knowlo Library'
         let tdescription = description || 'Knowlo is a learning community and platform for optimizing access to insight.'
-        let turl = url || 'https://i1.wp.com/knowlo.org/wp-content/uploads/2020/02/size-time-tight.png' // curriculum image hosted on wordpress site
+        let timageUrl = imageUrl || 'https://i1.wp.com/knowlo.org/wp-content/uploads/2020/02/size-time-tight.png' // curriculum image hosted on wordpress site
+        let turl = url || 'https://www.knowlo.io'
 
         let metaTemplate = `
         <!doctype html>
@@ -40,22 +42,22 @@ module.exports = function(linkPreview, db){
                 <title>${ttitle} - Knowlo</title>
                 <!-- Facebook meta -->
                 <meta name='og:title' content='${ttitle} - Knowlo'>
-                <meta name='og:description' content='${tdescription}>
-                <meta name='og:image' content='${turl}'>
+                <meta name='og:description' content='${tdescription}'>
+                <meta name='og:image' content='${timagUurl}'>
                 <meta name='og:url' content='${turl}'>
                 <meta name='og:type' content='website'>
                 <!-- Twitter meta -->
                 <meta name='twitter:card' content='summary_large_image'>
                 <meta name='twitter:title' content='${ttitle} - Knowlo'>
                 <meta name='twitter:description' content='${tdescription}'>
-                <meta name='twitter:image' content='${turl}'>
+                <meta name='twitter:image' content='${timagUurl}'>
                 <!-- Google / Search Engine meta -->
                 <meta name='name' content='${ttitle} - Knowlo'>
                 <meta name='description' content='${tdescription}'>
-                <meta name='image' content='${turl}'>
+                <meta name='image' content='${timagUurl}'>
             </head>
             <body>
-                <h1>${ttitle} - Knowlo</h1
+                <h1>${ttitle} - Knowlo</h1>
             </body>
         </html>
         `
